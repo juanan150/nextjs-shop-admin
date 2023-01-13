1eb8b652cfe672e86c9e00843aebe2bc7a174007
juanan0309 <juan.quiroz@wizeline.com>
2023-01-13 16:09:26 -0500

    setup tailwind & postcss

---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  semi: true,
+  singleQuote: false,
+  printWidth: 200,
+  tabWidth: 2,
+  trailingComma: "es5",
+  "prettier/prettier": ["error", {}, { usePrettierrc: false }],
+};
