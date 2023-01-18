19632d608bef3ceead38eaeae15bc3160bdf44c6
juanan0309 <juan.quiroz@wizeline.com>
2023-01-18 13:07:19 -0500

    first steps to have a login page

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,13 +1,14 @@
+import { ProviderAuth } from '@hooks/useAuth';
 import MainLayout from "@layout/MainLayout";
 import "@styles/tailwind.css";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <ProviderAuth>
       <MainLayout>
         <Component {...pageProps} />
       </MainLayout>
-    </>
+    </ProviderAuth>
   );
 }
 
---
+++ src/services/api/index.js
@@ -0,0 +1,35 @@
+const API = process.env.NEXT_PUBLIC_API_URL;
+const VERSION = process.env.NEXT_PUBLIC_API_VERSION;
+
+
+const endpoints = {
+  auth: {
+    login: `${API}/${VERSION}/auth/login`,
+    profile: `${API}/${VERSION}/auth/profile`
+  },
+  products: {
+    getProduct: (id) => `${API}/${VERSION}/products/${id}`,
+    updateProduct: (id) => `${API}/${VERSION}/products/${id}`,
+    deleteProduct: (id) => `${API}/${VERSION}/products/${id}`,
+    getProducts: `${API}/${VERSION}/products`,
+    addProducts: `${API}/${VERSION}/products`,
+  },
+  users: {
+    getUsers: `${API}/${VERSION}/users`,
+    getUser: (id) => `${API}/${VERSION}/users/${id}`,
+    updateUser: (id) => `${API}/${VERSION}/users/${id}`,
+    deleteUser: (id) => `${API}/${VERSION}/users/${id}`,
+  },
+  categories: {
+    getCategories: `${API}/${VERSION}/categories`,
+    addCategory: `${API}/${VERSION}/categories`,
+    getCategory: (id) => `${API}/${VERSION}/categories/${id}`,
+    updateCategory: (id) => `${API}/${VERSION}/categories/${id}`,
+    deleteCategory: (id) => `${API}/${VERSION}/categories/${id}`,
+  },
+  files: {
+    addImage: `${API}/${VERSION}/files/upload`
+  }
+};
+
+export default endpoints;
